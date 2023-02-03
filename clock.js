daeff4aced0096b26bfda0c269c4f1c31e81b3e6
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-02-03 10:39:46 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,25 @@
+const secondHand = document.querySelector('.sec-hand');
+// console.log(secondHand)
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+
+function setDate() {
+ const now = new Date();
+
+const seconds = now.getSeconds();
+const secondsDegrees = ((seconds / 60) * 360) + 90;
+secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+
+const mins = now.getMinutes();
+const minsDegrees = ((mins / 60) * 360) + 90;
+minHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+const hour = now.getMinutes();
+const hourDegrees = ((mins / 12) * 360) + 90;
+hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+}
+
+setInterval(setDate,1000);
